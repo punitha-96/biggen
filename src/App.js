c1c01e7eb0628437ed21e3b36b2ad5243042eb45
punitha-96 <punithakathirvel6@gmail.com>
2024-05-25 13:08:04 +0530

    code changed

--- src/App.js
+++ src/App.js
@@ -5,19 +5,12 @@ import Header from "./Header";
 import CardOne from "./CardOne";
 import CardTwo from "./CardTwo";
 import { Container, Row, Col } from "react-bootstrap";
-import Slider from "react-slick";
+
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 function App() {
   // Slider settings
-  const sliderSettings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
 
   return (
     <div className="App">
